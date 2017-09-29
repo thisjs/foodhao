779a657a9b63319176fabe144a1d5e047bfeec1d
duqichao <duqichao@jd.com>
2017-09-29 21:01:49 +0800

    更新

--- pages/index/index.js
+++ pages/index/index.js
@@ -11,31 +11,27 @@ Page({
                 goodsId:123,
                 img: './testImg/timg.jpg',
                 info:'杂粮煎饼：白面、小米面、豆面制作',
-                newPrice: '14.5',
-                oldPrice: '24',
-                isSelect: false
+                newPrice: 14.5,
+                oldPrice: 24,
+                isSelect: false,
+                selectNum: 0
             },
             {
                 goodsId:456,
                 img: './testImg/timg.jpg',
                 info:'杂粮煎饼：白面、小米面、豆面制作 +薄脆+肠',
-                newPrice: '14.5',
-                oldPrice: '24',
-                isSelect: false
+                newPrice: 14.5,
+                oldPrice: 24,
+                isSelect: false,
+                selectNum: 0
             }
         ],
 
-        addedList:[
-            /*{
-                goodsId:123,
-                img: './testImg/timg.jpg',
-                info:'杂粮煎饼：白面、小米面、豆面制作',
-                newPrice: '14.5',
-                oldPrice: '24',
-                addedNum: 1
-            }*/
-        ]
+        cartGoodsNum:0,
+        cartPrice:0,
+        addedList:[]
     },
+    // 点击购物车图标弹出列表
     tapCart(){
         if(this.data.slideUp){
             this.setData({
@@ -43,62 +39,114 @@ Page({
                 slideUpClass:''
             });
         }else{
+            if(app.globalData.addedList.length === 0) return;
             this.setData({
                 slideUp: true,
                 slideUpClass:'slide-up'
             });
         }
     },
+    // 添加购物车
     addGoodsAction(event){
         const goodsId = event.currentTarget.dataset.goodsid;
-        console.info(goodsId)
-    }
-  /*data: {
-    motto: 'Hello World',
-    userInfo: {},
-    hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo')
-  },
-  //事件处理函数
-  bindViewTap: function() {
-    wx.navigateTo({
-      url: '../logs/logs'
-    })
-  },
-  onLoad: function () {
-    if (app.globalData.userInfo) {
-      this.setData({
-        userInfo: app.globalData.userInfo,
-        hasUserInfo: true
-      })
-    } else if (this.data.canIUse){
-      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-      // 所以此处加入 callback 以防止这种情况
-      app.userInfoReadyCallback = res => {
-        this.setData({
-          userInfo: res.userInfo,
-          hasUserInfo: true
-        })
-      }
-    } else {
-      // 在没有 open-type=getUserInfo 版本的兼容处理
-      wx.getUserInfo({
-        success: res => {
-          app.globalData.userInfo = res.userInfo
-          this.setData({
-            userInfo: res.userInfo,
-            hasUserInfo: true
-          })
+
+        // 检查已添加
+        const record = app.globalData.addedList.find(p => p.goodsId == goodsId);
+
+        // 已添加商品池没有记录
+        if(!record){
+            // 找到商品池商品
+            const targetGoods = this.data.goodsList.find((p) => {
+                // 标识已添加
+                if(p.goodsId === goodsId){
+                    p.isSelect = true;
+                    p.selectNum = 1
+                }
+                return p.goodsId === goodsId
+            });
+            // 商品池目标商品添加到已添加商品池
+            app.globalData.addedList.push(targetGoods)
         }
-      })
+        // 有记录
+        else{
+            app.globalData.addedList.forEach(p => {
+                if(p.goodsId === goodsId){
+                    p.selectNum += 1;
+                }
+            });
+            /*this.setData({
+                addedList:this.data.addedList
+            });*/
+        }
+        // 重新存储数据，商品页面数据
+        this.setData({
+            goodsList:this.data.goodsList
+        });
+
+        this.countAddedData();
+    },
+
+    // 购物车列表点击加减按钮
+    changeCart(event){
+        const goodsId = event.currentTarget.dataset.goodsid;
+        const type = event.currentTarget.dataset.type;
+        app.globalData.addedList.forEach((p, i) => {
+            if(p.goodsId === goodsId){
+                if(type === 'reduce'){
+                    p.selectNum -= 1;
+                    if(p.selectNum === 0){
+                        this.data.goodsList.find(goods => goods.goodsId === goodsId).isSelect = false;
+
+                        app.globalData.addedList.splice(i,1);
+                    }
+                }else if(type === 'add'){
+                    p.selectNum += 1;
+                }
+            }
+        });
+        /*this.setData({
+            addedList:this.data.addedList
+        });*/
+
+        this.setData({
+            goodsList: this.data.goodsList
+        })
+        console.info(this.data.goodsList)
+        this.countAddedData();
+    },
+    countAddedData(){
+        // 更新购物车里商品数量数据
+        let selectNum = 0;
+        let selectPrice = 0;
+        app.globalData.addedList.forEach(item => {
+            selectNum += item.selectNum;
+            selectPrice += item.newPrice * item.selectNum;
+        });
+        this.setData({
+            cartGoodsNum:selectNum,
+            cartPrice: selectPrice,
+            addedList: app.globalData.addedList
+        })
+
+        if(app.globalData.addedList.length === 0){
+            this.setData({
+                cartGoodsNum:0,
+                cartPrice: 0,
+                slideUp: false,
+                slideUpClass: ''
+            });
+        }
+    },
+    toOrder(){
+        if(app.globalData.addedList.length){
+            console.info(1)
+            wx.navigateTo({
+                //目的页面地址
+                url: '/pages/order/index',
+                success: function(res){
+
+                }
+            })
+        }
     }
-  },
-  getUserInfo: function(e) {
-    console.log(e)
-    app.globalData.userInfo = e.detail.userInfo
-    this.setData({
-      userInfo: e.detail.userInfo,
-      hasUserInfo: true
-    })
-  }*/
 })
---
+++ pages/order/index.js
@@ -0,0 +1,6 @@
+const app = getApp();
+Page({
+    onShow:function () {
+        console.info(app.globalData.addedList)
+    }
+});
